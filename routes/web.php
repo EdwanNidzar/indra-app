cf77606a9890048377ca1d624ac880ddac5c40c8
AdeIndra2002 <117018387+AdeIndra2002@users.noreply.github.com>
2024-03-17 02:41:35 +0800

    feat : template by ade

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('pages.main');
+});
+
+Route::get('/pengajuan', function () {
+    return view('PengajuanCuti.pengajuan');
 });
 
 Route::get('/dashboard', function () {
@@ -28,4 +32,4 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
