74607cfe43a4be9a90e3c2cea79ab7f709d13a6a
EdwanNidzar <56621669+EdwanNidzar@users.noreply.github.com>
2024-03-19 07:16:44 +0800

    feat : add photo on users

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -8,6 +8,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\View\View;
+use CloudinaryLabs\CloudinaryLaravel\Facades\Cloudinary;
 
 class ProfileController extends Controller
 {
@@ -22,19 +23,57 @@ class ProfileController extends Controller
     }
 
     /**
+     * Handle upload file to Cloudinary.
+     */
+    public function upload(Request $request)
+    {
+        $path = 'STIEI/Profile/';
+        $file = $request->file('photo')->getClientOriginalName();
+
+        $fileName = pathinfo($file, PATHINFO_FILENAME);
+
+        $publicId = date('Y-m-d_His') . '_' . $fileName;
+        $upload = Cloudinary::upload($request->file('photo')->getRealPath(), [
+            'folder' => $path,
+            'public_id' => $publicId
+        ]);
+
+        return $upload->getSecurePath(); // Get the secure URL directly
+    }
+
+    /**
      * Update the user's profile information.
      */
     public function update(ProfileUpdateRequest $request): RedirectResponse
     {
-        $request->user()->fill($request->validated());
+        $user = $request->user();
 
-        if ($request->user()->isDirty('email')) {
-            $request->user()->email_verified_at = null;
+        // Update basic profile information
+        $user->fill([
+            'name' => $request->input('name'),
+            'nomor' => $request->input('nomor'),
+            'email' => $request->input('email'),
+        ]);
+
+        // Handle photo upload if provided
+        if ($request->hasFile('photo')) {
+            $photoUrl = $this->upload($request); // Get the photo URL
+            // Check if upload is successful
+            if ($photoUrl) {
+                $user->photo = $photoUrl;
+            } else {
+                return Redirect::back()->with('error', 'Failed to upload photo');
+            }
         }
 
-        $request->user()->save();
+        if ($user->isDirty('email')) {
+            $user->email_verified_at = null;
+        }
 
-        return Redirect::route('profile.edit')->with('status', 'profile-updated');
+        // Save changes
+        $user->save();
+
+        return redirect()->route('profile.edit')->with('status', 'profile-updated');
     }
 
     /**
--- app/Models/User.php
+++ app/Models/User.php
@@ -23,6 +23,7 @@ class User extends Authenticatable
         'nomor',
         'email',
         'password',
+        'photo',
     ];
 
     /**
